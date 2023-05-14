3e4f8392b125464c3111d3c0adbf7cb7bd8f78f2
UwUmeowmeow <98915612+UwUmeowmeow@users.noreply.github.com>
2023-05-15 10:01:45 +1200

    Exit

--- Main menu.py
+++ Main menu.py
@@ -4,7 +4,14 @@ Add monster cards, delete monster cards, Configure monster card, Display monster
 
 import easygui
 
-title_output = "Hello"
-MSG = "Choose one of the following buttons"
-buttons = ["Display", "Add", "Delete", "Configure", "Search"]
-easygui.buttonbox(msg=MSG, title=title_output, choices=buttons)
+
+while True:
+    title_output = "Hello"
+    MSG = "Choose one of the following buttons"
+    buttons = ["Display", "Add", "Delete", "Configure", "Search", "Exit"]
+    select_button = easygui.buttonbox(msg=MSG, title=title_output, choices=buttons)
+
+    if select_button == "Exit":
+        easygui.msgbox("You Exit the program")
+        break
+    
